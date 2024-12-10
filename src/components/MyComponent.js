9fc1ece8cae890968a137b623eb03a9618d20f90
Nvtai <buiblue123@gmail.com>
2024-12-10 22:33:32 +0700

    nesting component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,70 +2,15 @@
 // functional component (thường được ưu tiên hơn, ngắn gọn hơn, dễ đọc hơn)
 
 import React from 'react';
+import UserInfor from './UserInfor';
 
 class MyComponent extends React.Component {
-
-    state = {
-        name: 'MeoDev',
-        age: 20,
-        address: 'Nghean'
-    };
-
-    handleClick(event) {
-
-        console.log('My name is', this.state.name);
-
-        // merge state => react class
-        // nó tự động merge state, không cần phải ghi đè lại toàn bộ state
-        // nó phân biệt được state nào thay đổi, state nào không thay đổi
-        this.setState( {
-            name: 'Nvtai24',
-            age: Math.floor(Math.random()*100) + 1
-            // age: 24,
-            // address: 'Hanoi'
-        })
-        
-        // this.setState( {
-        //     age: Math.floor(Math.random()*100) + 1
-        // })
-
-
-
-    }
-
-    handleHover(event) {
-        console.log(event.pageX);
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-        console.log(event, event.target.value);
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    }
 
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age} years old
-                <br/>
-                {/* <button onClick={(event) => {this.handleClick(event)} }>Click me</button> */}
-                {/* <button onMouseOver={this.handleHover}>Hover me</button> */}
-
-                <form onSubmit={ (event) => {this.handleOnSubmit(event)} }>
-                    <input type='text' 
-                    onChange={ (event) => {
-                        this.handleOnChangeInput(event)
-                        }
-                    }></input>
-                    <button>Submit</button>
-                </form>            
+                <UserInfor/>       
             </div>
         );
     }
---
+++ src/components/UserInfor.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+
+class UserInfor extends React.Component {
+    state = {
+        name: 'MeoDev',
+        age: 20,
+        address: 'Nghean'
+    };
+
+
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnChangeAge = (event) => {
+        // !!! BAD CODE
+        // cách code này phá vỡ quy trình quản lí state của react, không được phép thay đổi state trực tiếp
+        // this.state.age = event.target.value;
+        // luôn sử dụng setState để cập nhật state nhằm đảm bảo React hoạt động chính xác và UI được đồng bộ
+        this.setState({
+            age: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+
+    render() {
+        return (
+            <div>
+                My name is {this.state.name} and I'm {this.state.age} years old
+
+                <form onSubmit={ (event) => {this.handleOnSubmit(event)} }>
+                    <label>Your name:</label>
+                    <input type='text' 
+                    value={this.state.name}
+                    onChange={ (event) => {
+                        this.handleOnChangeInput(event)
+                        }
+                    }></input>
+
+                    <label>Your age:</label>
+                    <input type='text' 
+                    value={this.state.age}
+                    onChange={ (event) => {
+                        this.handleOnChangeAge(event)
+                        }
+                    }></input>
+                    <button>Submit</button>
+                </form>   
+
+            </div>
+        )
+    }
+
+
+}
+
+export default UserInfor;
