bf907a0b0986169eb9483c74a5e2ce18cd418983
Nvtai <buiblue123@gmail.com>
2024-12-10 15:31:00 +0700

    state object

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,14 +1,22 @@
 // class component
-// function component
+// functional component (thường được ưu tiên hơn, ngắn gọn hơn, dễ đọc hơn)
 
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'MeoDev',
+        age: 20,
+        address: 'Nghean'
+    };
+
+
     // JSX
     render() {
         return (
-            <div>My first component
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and I'm {this.state.age} years old
             </div>
         );
     }
