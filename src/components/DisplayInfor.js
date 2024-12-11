67c9e8930d0937a5c6dd2b2b7a0f22572b0c6de2
Nvtai <buiblue123@gmail.com>
2024-12-11 20:44:09 +0700

    summary 1

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,9 +3,14 @@ import './DisplayInfor.scss';
 import logo from '../logo.svg';
 
 class DisplayInfor extends React.Component {
-    state = {
-        isShowListUser: true
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isShowListUser: true
+        }
     }
+
 
     handleShowHide = () => {
         this.setState({
