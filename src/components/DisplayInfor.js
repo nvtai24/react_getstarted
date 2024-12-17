9a5d3adb9c94db149470e49555e2f8fe20a6a0e5
Nvtai <buiblue123@gmail.com>
2024-12-18 00:03:44 +0700

    useEffect hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,39 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg";
-
-// stateless vs stateful component
-// class DisplayInfor extends React.Component {
-//   render() {
-//     console.log("render");
-//     const { listUsers } = this.props;
-
-//     return (
-//       <div className="display-infor-container">
-//         {true && (
-//           <>
-//             {listUsers.map((user) => {
-//               return (
-//                 <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-//                   <div>My name is {user.name}</div>
-//                   <div>I'm {user.age} years old</div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfor = (props) => {
   const { listUsers } = props;
@@ -44,6 +11,15 @@ const DisplayInfor = (props) => {
   const handleShowHideListUser = () => {
     setShowHideListUser(!isShowHideListUser);
   };
+
+  console.log("call me render");
+
+  useEffect(() => {
+    if (listUsers.length == 0) {
+      alert("List user is empty");
+    }
+    console.log("call me useEffect");
+  }, [listUsers]);
 
   return (
     <div className="display-infor-container">
